d40007916d9035d1cc12887730c4b308420e1217
Ganeesya <ganephone@gmail.com>
2023-03-25 19:45:22 +0900

    パスの文字化け対応

--- VTubeStudioAccess/Class1.cs
+++ VTubeStudioAccess/Class1.cs
@@ -1,65 +1,18 @@
-﻿using System;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
-using VTS.Models;
+﻿using System.Text;
 
 namespace VTubeStudioAccess
 {
     public class VTubeStudioAccess
     {
-        // // 文字列単体：引数
-        // [DllExport]
-        // static int GetStringLength([MarshalAs(UnmanagedType.LPStr)]string s)
-        // {
-        //     return s.Length;
-        // }
-        //
-        // // 文字列：返値
-        // [DllExport][return: MarshalAs(UnmanagedType.LPStr)]
-        // static string GetCodeString()
-        // {
-        //     return "abcd".Replace("a","A");
-        // }
-
-        // [StructLayout(LayoutKind.Sequential)]
-        // struct Parameter
-        // {
-        //     [MarshalAs(UnmanagedType.LPStr)]
-        //     public string id;
-        //
-        //     public float value;
-        //     public float max;
-        //     public float min;
-        // }
-        
         private static VTSAccess access = new VTSAccess();
-        
-        // 構造体単体：返値
-        // [DllExport]
-        // static IntPtr GetParameter(int index)
-        // {
-        //     VTSParameter parameter = access.GetParameter(index);
-        //     Parameter ret;
-        //     ret.id = parameter.name;
-        //     ret.value = parameter.value;
-        //     ret.max = parameter.max;
-        //     ret.min = parameter.min;
-        //
-        //     IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Parameter)));
-        //     Marshal.StructureToPtr(ret, ptr, false);
-        //     return ptr;
-        // }
-        //
-        // [DllExport]
-        // static int GetParameterCount()
-        // {
-        //     return access.getParameterCount();
-        // }
 
         [DllExport]
         static void StockParameterFromVTS(string logBaseDirPath, float fps)
         {
-            access.StockParameterFromVTS(logBaseDirPath,  fps);
+            string encordedPath = Encoding.UTF8.GetString( Encoding.GetEncoding("Shift_JIS").GetBytes(logBaseDirPath) );
+            // Debug.Print("preEncode path:"+logBaseDirPath);
+            // Debug.Print("encode path:"+encordedPath);
+            access.StockParameterFromVTS(encordedPath,  fps);
         }
 
         [DllExport]
